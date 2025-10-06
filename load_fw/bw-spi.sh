a79f5c0ca7e5bd94fa9b0860f448211a24f05b62
Shane MacPhillamy <rcoaic@gmail.com>
2025-10-06 11:01:18 +1030

    Part way through; FlashROM still failing, getting all zeros

--- load_fw/bw-spi.sh
+++ load_fw/bw-spi.sh
@@ -8,20 +8,36 @@ echo -e "\n|--------------------------------------------|"
 echo -e "|-----Flashing Beaglewire with SPI MODE -----|"
 echo -e "|--------------------------------------------|\n\n"
 
-echo -e "Truncating file to max limit of 4194304\n"
-truncate $1 -s 4194304 
+#echo -e "Truncating file to max limit of 4194304\n"
+#truncate $1 -s 4194304 
+FILE=$1
+MAX=4194304
+SIZE=$(stat -c%s "$FILE")
 
-echo -e "Turning of FPGA by keeping reset High"
-config-pin P9_25 gpio_pu
-echo 117 > /sys/class/gpio/export || echo 117 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio117/direction || echo out > /sys/class/gpio/gpio117/direction
+if [ "$SIZE" -gt "$MAX" ]; then
+    echo "Error: $FILE is too large ($SIZE > $MAX bytes)"
+    exit 1
+fi
 
+EXIT 0
+
+echo -e "Turning off FPGA by keeping reset High"
+gpioset -c gpiochip2 -b pull-up -p 1ms -t 0 21=0
+# Run gpioset in background, keep PID
+gpioset -c gpiochip2 21=1 &
+GPIOSET_PID=$!
+gpioget -c gpiochip2 21
+#gpioset -c gpiochip2 -p 1ms -t 0 21=1   # pulse high then exit
+#gpioset -c gpiochip2 -p 1ms -t 0 21=0   # pulse low then exit
+gpioget -c gpiochip2 21
 echo -e "Activating SPI mode"
-config-pin P9_28 spi_cs
-config-pin P9_31 spi_sclk
+#config-pin P9_28 spi_cs
+#config-pin P9_31 spi_sclk
 
-echo -e "Activating the flash chip with 0xAB command\n"
-spi_test -d /dev/spidev1.0 -l 1 -m ab 
+#echo -e "Activating the flash chip with 0xAB command\n"
+echo -e "Activating the flash chip; Transmit the byte \xab data\n"
+#spi_test -d /dev/spidev1.0 -l 1 -m ab 
+sudo ./spidev_test -D /dev/spidev1.0 -s 500000 -p "\xaa"
 
 echo -e "\n|--------------------------------------------|"
 echo -e "|---------  Programming nor flash -----------|"
